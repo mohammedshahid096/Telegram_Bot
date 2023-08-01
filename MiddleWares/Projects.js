340e8c153041ca9a7dce3b17b4320ffad8a98951
Mohammed shahid <84904302+mohammedshahid096@users.noreply.github.com>
2023-08-01 17:20:03 +0530

    Added Telegram Bot using node js

---
+++ MiddleWares/Projects.js
@@ -0,0 +1,30 @@
+const projects = [
+  { name: "E-Commerce Project", url: "https://e-commerce-sgq0.onrender.com/" },
+  { name: "Protfolio", url: "https://mohdshahidportfolio.netlify.app/" },
+  {
+    name: "College Learning Guide using React",
+    url: "https://learningguidence.netlify.app/programming/home",
+  },
+  {
+    name: "Wheather Application using React",
+    url: "https://mohammedshahid096.github.io/reactwheatherapplication/",
+  },
+  {
+    name: "Contact Mang using React",
+    url: "https://mohammedshahid096reactcontactmanagement.netlify.app/",
+  },
+  {
+    name: "Upgrad Scribber using js",
+    url: "https://mohammedshahid096.github.io/upGrad_project_scribbler/",
+  },
+  {
+    name: "Simple Calculator using JS",
+    url: "https://mohammedshahid096.github.io/calculator/",
+  },
+  {
+    name: "Rock Paper Scissor using JS",
+    url: "https://mohammedshahid096.github.io/rockpapersicssorjavascript/",
+  },
+];
+
+module.exports = projects;
---
+++ index.js
@@ -0,0 +1,158 @@
+const TelegramBot = require("node-telegram-bot-api");
+const dotenv = require("dotenv");
+const axios = require("axios");
+const Projects = require("./MiddleWares/Projects");
+// !---------------------------------
+
+dotenv.config({ path: "./Config/config.env" });
+const ShahidALT_Bot = new TelegramBot(process.env.BOT_TOKEN, {
+  polling: true,
+});
+
+// !---------------------------------
+// TODO : for /start
+ShahidALT_Bot.onText(/\/start/, (message) => {
+  try {
+    const chatID = message.from.id;
+    ShahidALT_Bot.sendMessage(
+      chatID,
+      `hello how are you ${message.from.first_name} ${message.from.last_name}`
+    );
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+// TODO : for /for menu
+ShahidALT_Bot.onText(/\/menu/, (message) => {
+  try {
+    const chatID = message.from.id;
+    MenuList = "1. /Repository \n2. /Test \n3. /Projects";
+    ShahidALT_Bot.sendMessage(chatID, MenuList);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+// TODO : for /Repositories
+ShahidALT_Bot.onText(/\Repository/, async (message) => {
+  try {
+    const chatID = message.from.id;
+
+    ShahidALT_Bot.sendChatAction(chatID, "typing");
+
+    let api_url = "https://api.github.com/users/mohammedshahid096/repos";
+
+    const { data } = await axios.get(api_url);
+
+    let getData = data.map((item, index) =>
+      item.html_url.replace(
+        "https://github.com/mohammedshahid096/",
+        `${index + 1}. `
+      )
+    );
+    getData = getData.toString();
+    getData = getData.replaceAll(",", "\n");
+    let response = "---All Repositories--- \n\n" + getData;
+    ShahidALT_Bot.sendMessage(chatID, response);
+    ShahidALT_Bot.sendMessage(chatID, "response : /repo Choice_number");
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+ShahidALT_Bot.onText(/\/repo (.+)/, async (msg, match) => {
+  try {
+    const chatID = msg.from.id;
+    ShahidALT_Bot.sendChatAction(chatID, "typing");
+
+    let choiceSelection = match[1];
+
+    choiceSelection = parseInt(choiceSelection);
+    choiceSelection = choiceSelection - 1;
+
+    let api_url = "https://api.github.com/users/mohammedshahid096/repos";
+
+    const { data } = await axios.get(api_url);
+    const link = data[choiceSelection].html_url;
+
+    ShahidALT_Bot.sendMessage(chatID, link);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+ShahidALT_Bot.onText(/\/dictionary/, (message) => {
+  try {
+    const chatID = message.from.id;
+    let m =
+      "<b>Hi Am a Dictionary \nsearch.... in this way</b> \n\n example : \n/Dict amazing";
+    ShahidALT_Bot.sendMessage(chatID, m, { parse_mode: "HTML" });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+ShahidALT_Bot.onText(/\/Dict (.+)/, async (msg, match) => {
+  try {
+    const chatID = msg.from.id;
+    ShahidALT_Bot.sendChatAction(chatID, "typing");
+    let choiceSelection = match[1];
+    const search_url = `https://api.dictionaryapi.dev/api/v2/entries/en/${choiceSelection}`;
+    const { data } = await axios.get(search_url);
+    let word = data[0].word;
+    let partOfSpeech = data[0].meanings[0].partOfSpeech;
+    let definition = data[0].meanings[0].definitions[0].definition;
+    let synonyms = data[0].meanings[0].synonyms.toString();
+
+    const details = `<b>Word :</b> ${word} \n<b>parts of speech : </b> ${partOfSpeech} \n<b>Definition : </b> ${definition} \n <b>Synonyms : </b> ${synonyms}`;
+
+    ShahidALT_Bot.sendMessage(chatID, details, { parse_mode: "HTML" });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+ShahidALT_Bot.onText(/\/linkedin/, (message) => {
+  try {
+    const chatID = message.from.id;
+    const linkedln = "https://www.linkedin.com/in/mohammed-shahid-9aa61222a";
+    ShahidALT_Bot.sendMessage(chatID, linkedln);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+ShahidALT_Bot.onText(/\/Projects/, (message) => {
+  try {
+    const chatID = message.from.id;
+    let reply = "";
+    for (let item in Projects) {
+      reply += "" + item + ". " + Projects[item].name + "\n";
+    }
+
+    ShahidALT_Bot.sendMessage(chatID, reply);
+    ShahidALT_Bot.sendMessage(
+      chatID,
+      "<b>send the message in this format :</b> \n\n/project your_option",
+      { parse_mode: "HTML" }
+    );
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+ShahidALT_Bot.onText(/\/project (.+)/, (message, match) => {
+  try {
+    const chatID = message.from.id;
+    ShahidALT_Bot.sendChatAction(chatID, "typing");
+    let choiceSelection = match[1];
+    choiceSelection = parseInt(choiceSelection);
+    const link = Projects[choiceSelection].url;
+    ShahidALT_Bot.sendMessage(chatID, link);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+console.log("node server is started");
